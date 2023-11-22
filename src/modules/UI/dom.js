c52ee7f95149215cef3cae88af1e16e4d411e11f
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-11-22 20:19:45 +0100

    add getComputerBoard
    create computerBoard with randomly placed ships

--- src/modules/UI/dom.js
+++ src/modules/UI/dom.js
@@ -20,12 +20,86 @@ humanBoard.placeShipX(ship(4, '4'), [0, 6]);
 humanBoard.placeShipX(ship(5, '5'), [0, 8]);
 
 const computer = player('computer');
-const computerBoard = gameBoard();
-computerBoard.placeShipX(ship(2, '1'), [0, 0]);
-computerBoard.placeShipX(ship(3, '2'), [0, 2]);
-computerBoard.placeShipX(ship(3, '3'), [0, 4]);
-computerBoard.placeShipX(ship(4, '4'), [0, 6]);
-computerBoard.placeShipX(ship(5, '5'), [0, 8]);
+const computerShips = [
+  ship(2, '1'),
+  ship(3, '2'),
+  ship(3, '3'),
+  ship(4, '4'),
+  ship(5, '5'),
+];
+
+const getComputerBoard = () => {
+  // make placeShip only one method that can accept x and y.
+  // Place first ship at random coord and make coordinates around it occupied "X"
+  const computerBoard = gameBoard();
+  const illegalSquares = [];
+
+  computerShips.forEach((ship) => {
+    const radNum = Math.floor(Math.random() * 2); // 0 for x-placement and 1 for y
+    let valid = true;
+    while (valid) {
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10);
+
+      if (ship.getLength() === 2) {
+        if (radNum === 0) {
+          computerBoard.placeShipX(ship, [x, y]);
+          for (let i = y - 1; i <= y + 1; i++) {
+            for (let j = x - 1; j <= x + ship.getLength(); j++) {
+              if (!illegalSquares.includes(`${j},${i}`))
+                illegalSquares.push(`${j},${i}`);
+            }
+          }
+        } else {
+          computerBoard.placeShipY(ship, [x, y]);
+          for (let i = y - 1; i <= y + ship.getLength(); i++) {
+            for (let j = x - 1; j <= x + 1; j++) {
+              if (!illegalSquares.includes(`${j},${i}`))
+                illegalSquares.push(`${j},${i}`);
+            }
+          }
+        }
+        break;
+      }
+      if (illegalSquares.includes(`${x},${y}`)) continue;
+      if (radNum === 0) {
+        if (
+          illegalSquares.includes(`${x + ship.getLength() - 1},${y}`) ||
+          illegalSquares.includes(`${x + ship.getLength() - 2},${y}`) ||
+          illegalSquares.includes(`${x + ship.getLength() - 3},${y}`) ||
+          x + ship.getLength() - 1 > 9
+        )
+          continue;
+        computerBoard.placeShipX(ship, [x, y]);
+        for (let i = y - 1; i <= y + 1; i++) {
+          for (let j = x - 1; j <= x + ship.getLength(); j++) {
+            if (!illegalSquares.includes(`${j},${i}`))
+              illegalSquares.push(`${j},${i}`);
+          }
+        }
+      } else {
+        if (
+          illegalSquares.includes(`${x},${y + ship.getLength() - 1}`) ||
+          illegalSquares.includes(`${x},${y + ship.getLength() - 2}`) ||
+          illegalSquares.includes(`${x},${y + ship.getLength() - 3}`) ||
+          y + ship.getLength() - 1 > 9
+        )
+          continue;
+        computerBoard.placeShipY(ship, [x, y]);
+        for (let i = y - 1; i <= y + ship.getLength(); i++) {
+          for (let j = x - 1; j <= x + 1; j++) {
+            if (!illegalSquares.includes(`${j},${i}`))
+              illegalSquares.push(`${j},${i}`);
+          }
+        }
+      }
+      break;
+    }
+  });
+  return computerBoard;
+};
+
+const computerBoard = getComputerBoard();
 
 const renderhumanBoard = () => {
   const hBoard = document.querySelector('#human-board');
@@ -80,7 +154,7 @@ const renderComputerBoard = () => {
       } else if (computerBoard.getBoard()[i][j] === 'h') {
         square.classList.add('hit');
       } else {
-        square.classList.add('hiddenShip');
+        square.classList.add('hidden-ship');
         square.addEventListener('click', attackSquare);
       }
       square.setAttribute('id', `${[j, i]}`);
