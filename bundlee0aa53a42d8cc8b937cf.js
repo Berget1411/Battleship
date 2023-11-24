df18c76cc27b0433b8c5254f1f5e2c80409a5228
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-11-24 11:24:23 +0100

    user can start game with random ship placements

--- bundlee0aa53a42d8cc8b937cf.js
+++ bundlee0aa53a42d8cc8b937cf.js
@@ -8,10 +8,11 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_UI_dom_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/UI/dom.js */ "./src/modules/UI/dom.js");
+/* harmony import */ var _modules_UI_dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/UI/dom */ "./src/modules/UI/dom.js");
 /* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/main.scss */ "./src/styles/main.scss");
 
 
+_modules_UI_dom__WEBPACK_IMPORTED_MODULE_0__["default"].getStartScreen();
 
 /***/ }),
 
@@ -22,92 +23,124 @@ __webpack_require__.r(__webpack_exports__);
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
 /* harmony import */ var _factories_ship__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../factories/ship */ "./src/modules/factories/ship.js");
 /* harmony import */ var _factories_player__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../factories/player */ "./src/modules/factories/player.js");
 /* harmony import */ var _factories_gameboard__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../factories/gameboard */ "./src/modules/factories/gameboard.js");
 
 
 
-var gameStatus = true;
-var counter = 0;
-var ship1 = (0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(2, '1');
-var ship2 = (0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(3, '2');
-var ship3 = (0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(3, '3');
-var ship4 = (0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(4, '4');
-var ship5 = (0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(5, '5');
-var human = (0,_factories_player__WEBPACK_IMPORTED_MODULE_1__["default"])('human');
-var humanBoard = (0,_factories_gameboard__WEBPACK_IMPORTED_MODULE_2__["default"])();
-humanBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(2, '1'), [0, 0]);
-humanBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(3, '2'), [0, 2]);
-humanBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(3, '3'), [0, 4]);
-humanBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(4, '4'), [0, 6]);
-humanBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(5, '5'), [0, 8]);
-var computer = (0,_factories_player__WEBPACK_IMPORTED_MODULE_1__["default"])('computer');
-var computerBoard = (0,_factories_gameboard__WEBPACK_IMPORTED_MODULE_2__["default"])();
-computerBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(2, '1'), [0, 0]);
-computerBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(3, '2'), [0, 2]);
-computerBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(3, '3'), [0, 4]);
-computerBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(4, '4'), [0, 6]);
-computerBoard.placeShipX((0,_factories_ship__WEBPACK_IMPORTED_MODULE_0__["default"])(5, '5'), [0, 8]);
-var renderhumanBoard = function renderhumanBoard() {
-  var hBoard = document.querySelector('#human-board');
-  for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
-      var square = document.createElement('div');
-      if (humanBoard.getBoard()[i][j] === 'x') {
-        square.classList.add('empty');
-      } else if (humanBoard.getBoard()[i][j] === 'm') {
-        square.classList.add('missed');
-      } else if (humanBoard.getBoard()[i][j] === 'h') {
-        square.classList.add('hit');
-      } else {
-        square.classList.add('ship');
-      }
-      square.setAttribute('id', "".concat([j, i]));
-      hBoard.append(square);
-    }
-  }
-};
-var renderComputerBoard = function renderComputerBoard() {
-  var attackSquare = function attackSquare(e) {
-    if (!humanBoard.isAllSunk() && !computerBoard.isAllSunk()) {
-      human.attack(computerBoard, e.target.id.split(','));
-      computer.attack(humanBoard);
-      document.querySelector('#human-board').textContent = '';
-      document.querySelector('#computer-board').textContent = '';
-      renderhumanBoard();
+var dom = function () {
+  var gameStatus = true;
+  var counter = 0;
+  var human = (0,_factories_player__WEBPACK_IMPORTED_MODULE_1__["default"])('human');
+  var humanBoard;
+  var computer = (0,_factories_player__WEBPACK_IMPORTED_MODULE_1__["default"])('computer');
+  var computerBoard;
+  var startScreen = document.querySelector('#start');
+  var gameScreen = document.querySelector('#game');
+  var getStartScreen = function getStartScreen() {
+    humanBoard = (0,_factories_gameboard__WEBPACK_IMPORTED_MODULE_2__.gameBoard)();
+    renderStartBoard();
+    var clickedfunc = function clickedfunc() {
+      humanBoard = (0,_factories_gameboard__WEBPACK_IMPORTED_MODULE_2__.getRandomBoard)();
+      computerBoard = (0,_factories_gameboard__WEBPACK_IMPORTED_MODULE_2__.getRandomBoard)();
+      renderHumanBoard();
       renderComputerBoard();
-      if (humanBoard.isAllSunk() && computerBoard.isAllSunk()) {
-        console.log('Draw!');
-      } else if (computerBoard.isAllSunk()) {
-        console.log('Human won!');
-      } else if (humanBoard.isAllSunk()) {
-        console.log('Computer won!');
+      document.querySelector('#start').classList.toggle('hidden');
+      document.querySelector('#game').classList.toggle('hidden');
+    };
+    document.querySelector('#random-ship-placements').addEventListener('click', clickedfunc);
+  };
+  var startGame = function startGame() {
+    startScreen.classList.toggle('hidden');
+    renderHumanBoard();
+    renderComputerBoard();
+    gameScreen.classList.toggle('hidden');
+  };
+  var renderHumanBoard = function renderHumanBoard() {
+    var hBoard = document.querySelector('#human-board');
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        var square = document.createElement('div');
+        if (humanBoard.getBoard()[i][j] === 'x') {
+          square.classList.add('empty');
+        } else if (humanBoard.getBoard()[i][j] === 'm') {
+          square.classList.add('missed');
+        } else if (humanBoard.getBoard()[i][j] === 'h') {
+          square.classList.add('hit');
+        } else {
+          square.classList.add('ship');
+        }
+        square.setAttribute('id', "".concat([j, i]));
+        hBoard.append(square);
       }
     }
   };
-  var cBoard = document.querySelector('#computer-board');
-  for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
-      var square = document.createElement('div');
-      if (computerBoard.getBoard()[i][j] === 'x') {
-        square.classList.add('empty');
-        square.addEventListener('click', attackSquare);
-      } else if (computerBoard.getBoard()[i][j] === 'm') {
-        square.classList.add('missed');
-      } else if (computerBoard.getBoard()[i][j] === 'h') {
-        square.classList.add('hit');
-      } else {
-        square.classList.add('hiddenShip');
-        square.addEventListener('click', attackSquare);
+  var renderComputerBoard = function renderComputerBoard() {
+    var cBoard = document.querySelector('#computer-board');
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        var square = document.createElement('div');
+        if (computerBoard.getBoard()[i][j] === 'x') {
+          square.classList.add('empty');
+          square.addEventListener('click', attackSquare);
+        } else if (computerBoard.getBoard()[i][j] === 'm') {
+          square.classList.add('missed');
+        } else if (computerBoard.getBoard()[i][j] === 'h') {
+          square.classList.add('hit');
+        } else {
+          square.classList.add('ship');
+          square.addEventListener('click', attackSquare);
+        }
+        square.setAttribute('id', "".concat([j, i]));
+        cBoard.append(square);
       }
-      square.setAttribute('id', "".concat([j, i]));
-      cBoard.append(square);
     }
-  }
-};
-renderhumanBoard();
-renderComputerBoard();
+    function attackSquare(e) {
+      if (!humanBoard.isAllSunk() && !computerBoard.isAllSunk()) {
+        human.attack(computerBoard, e.target.id.split(','));
+        computer.attack(humanBoard);
+        document.querySelector('#human-board').textContent = '';
+        document.querySelector('#computer-board').textContent = '';
+        renderHumanBoard();
+        renderComputerBoard();
+        if (humanBoard.isAllSunk() && computerBoard.isAllSunk()) {
+          console.log('Draw!');
+        } else if (computerBoard.isAllSunk()) {
+          console.log('Human won!');
+        } else if (humanBoard.isAllSunk()) {
+          console.log('Computer won!');
+        }
+      }
+    }
+  };
+  var renderStartBoard = function renderStartBoard() {
+    var startBoard = document.querySelector('#start-board');
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        var square = document.createElement('div');
+        if (humanBoard.getBoard()[i][j] === 'x') {
+          square.classList.add('empty');
+        } else if (humanBoard.getBoard()[i][j] === 'm') {
+          square.classList.add('missed');
+        } else if (humanBoard.getBoard()[i][j] === 'h') {
+          square.classList.add('hit');
+        } else {
+          square.classList.add('ship');
+        }
+        square.setAttribute('id', "".concat([j, i]));
+        startBoard.append(square);
+      }
+    }
+  };
+  return {
+    getStartScreen: getStartScreen
+  };
+}();
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (dom);
 
 /***/ }),
 
@@ -119,8 +152,10 @@ renderComputerBoard();
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   gameBoard: () => (/* binding */ gameBoard),
+/* harmony export */   getRandomBoard: () => (/* binding */ getRandomBoard)
 /* harmony export */ });
+/* harmony import */ var _ship__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ship */ "./src/modules/factories/ship.js");
 function _slicedToArray(arr, i) {
   return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
 }
@@ -170,6 +205,7 @@ function _iterableToArrayLimit(r, l) {
 function _arrayWithHoles(arr) {
   if (Array.isArray(arr)) return arr;
 }
+
 var createBoard = function createBoard() {
   var board = [];
   for (var i = 0; i < 10; i++) {
@@ -257,7 +293,61 @@ var gameBoard = function gameBoard() {
     isAllSunk: isAllSunk
   };
 };
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (gameBoard);
+var getRandomBoard = function getRandomBoard() {
+  var board = gameBoard();
+  var ships = (0,_ship__WEBPACK_IMPORTED_MODULE_0__.getShips)();
+  var illegalSquares = [];
+  for (var i in ships) {
+    var ship = ships[i];
+    var radNum = Math.floor(Math.random() * 2); // 0 for x-placement and 1 for y
+    var valid = true;
+    while (valid) {
+      var x = Math.floor(Math.random() * 10);
+      var y = Math.floor(Math.random() * 10);
+      if (ship.getLength() === 2) {
+        if (radNum === 0) {
+          if (x + ship.getLength() - 1 > 9) continue;
+          board.placeShipX(ship, [x, y]);
+          for (var _i2 = y - 1; _i2 <= y + 1; _i2++) {
+            for (var j = x - 1; j <= x + ship.getLength(); j++) {
+              if (!illegalSquares.includes("".concat(j, ",").concat(_i2))) illegalSquares.push("".concat(j, ",").concat(_i2));
+            }
+          }
+        } else {
+          if (y + ship.getLength() - 1 > 9) continue;
+          board.placeShipY(ship, [x, y]);
+          for (var _i3 = y - 1; _i3 <= y + ship.getLength(); _i3++) {
+            for (var _j = x - 1; _j <= x + 1; _j++) {
+              if (!illegalSquares.includes("".concat(_j, ",").concat(_i3))) illegalSquares.push("".concat(_j, ",").concat(_i3));
+            }
+          }
+        }
+        break;
+      }
+      if (illegalSquares.includes("".concat(x, ",").concat(y))) continue;
+      if (radNum === 0) {
+        if (illegalSquares.includes("".concat(x + ship.getLength() - 1, ",").concat(y)) || illegalSquares.includes("".concat(x + ship.getLength() - 2, ",").concat(y)) || illegalSquares.includes("".concat(x + ship.getLength() - 3, ",").concat(y)) || x + ship.getLength() - 1 > 9) continue;
+        board.placeShipX(ship, [x, y]);
+        for (var _i4 = y - 1; _i4 <= y + 1; _i4++) {
+          for (var _j2 = x - 1; _j2 <= x + ship.getLength(); _j2++) {
+            if (!illegalSquares.includes("".concat(_j2, ",").concat(_i4))) illegalSquares.push("".concat(_j2, ",").concat(_i4));
+          }
+        }
+      } else {
+        if (illegalSquares.includes("".concat(x, ",").concat(y + ship.getLength() - 1)) || illegalSquares.includes("".concat(x, ",").concat(y + ship.getLength() - 2)) || illegalSquares.includes("".concat(x, ",").concat(y + ship.getLength() - 3)) || y + ship.getLength() - 1 > 9) continue;
+        board.placeShipY(ship, [x, y]);
+        for (var _i5 = y - 1; _i5 <= y + ship.getLength(); _i5++) {
+          for (var _j3 = x - 1; _j3 <= x + 1; _j3++) {
+            if (!illegalSquares.includes("".concat(_j3, ",").concat(_i5))) illegalSquares.push("".concat(_j3, ",").concat(_i5));
+          }
+        }
+      }
+      break;
+    }
+  }
+  return board;
+};
+
 
 /***/ }),
 
@@ -357,7 +447,8 @@ var player = function player(type) {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   getShips: () => (/* binding */ getShips),
+/* harmony export */   ship: () => (/* binding */ ship)
 /* harmony export */ });
 var ship = function ship(shipLength, id) {
   var length = shipLength;
@@ -384,7 +475,16 @@ var ship = function ship(shipLength, id) {
     isSunk: isSunk
   };
 };
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ship);
+var getShips = function getShips() {
+  return {
+    destroyer: ship(2, '1'),
+    submarine: ship(3, '2'),
+    cruiser: ship(3, '3'),
+    battleship: ship(4, '4'),
+    carrier: ship(5, '5')
+  };
+};
+
 
 /***/ }),
 
@@ -407,7 +507,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, `main {
+___CSS_LOADER_EXPORT___.push([module.id, `#game {
   display: flex;
   justify-content: center;
   gap: 40px;
@@ -436,7 +536,11 @@ ___CSS_LOADER_EXPORT___.push([module.id, `main {
 }
 .board .empty {
   background-color: white;
-}`, "",{"version":3,"sources":["webpack://./src/styles/main.scss"],"names":[],"mappings":"AAAA;EACE,aAAA;EACA,uBAAA;EACA,SAAA;AACF;;AAEA;EACE,YAAA;EACA,aAAA;EACA,uBAAA;EACA,aAAA;EACA,eAAA;AACF;AACE;EACE,WAAA;EACA,YAAA;EACA,uBAAA;AACJ;AAEE;EACE,uBAAA;AAAJ;AAGE;EACE,qBAAA;AADJ;AAIE;EACE,uBAAA;AAFJ;AAKE;EACE,uBAAA;AAHJ","sourcesContent":["main {\r\n  display: flex;\r\n  justify-content: center;\r\n  gap: 40px;\r\n}\r\n\r\n.board {\r\n  width: 300px;\r\n  height: 300px;\r\n  border: 1px solid black;\r\n  display: flex;\r\n  flex-wrap: wrap;\r\n\r\n  div {\r\n    width: 28px;\r\n    height: 28px;\r\n    border: 1px solid black;\r\n  }\r\n\r\n  .ship {\r\n    background-color: black;\r\n  }\r\n\r\n  .hit {\r\n    background-color: red;\r\n  }\r\n\r\n  .missed {\r\n    background-color: green;\r\n  }\r\n\r\n  .empty {\r\n    background-color: white;\r\n  }\r\n}\r\n"],"sourceRoot":""}]);
+}
+
+.hidden {
+  display: none !important;
+}`, "",{"version":3,"sources":["webpack://./src/styles/main.scss"],"names":[],"mappings":"AAAA;EACE,aAAA;EACA,uBAAA;EACA,SAAA;AACF;;AAEA;EACE,YAAA;EACA,aAAA;EACA,uBAAA;EACA,aAAA;EACA,eAAA;AACF;AACE;EACE,WAAA;EACA,YAAA;EACA,uBAAA;AACJ;AAEE;EACE,uBAAA;AAAJ;AAGE;EACE,qBAAA;AADJ;AAIE;EACE,uBAAA;AAFJ;AAKE;EACE,uBAAA;AAHJ;;AAOA;EACE,wBAAA;AAJF","sourcesContent":["#game {\r\n  display: flex;\r\n  justify-content: center;\r\n  gap: 40px;\r\n}\r\n\r\n.board {\r\n  width: 300px;\r\n  height: 300px;\r\n  border: 1px solid black;\r\n  display: flex;\r\n  flex-wrap: wrap;\r\n\r\n  div {\r\n    width: 28px;\r\n    height: 28px;\r\n    border: 1px solid black;\r\n  }\r\n\r\n  .ship {\r\n    background-color: black;\r\n  }\r\n\r\n  .hit {\r\n    background-color: red;\r\n  }\r\n\r\n  .missed {\r\n    background-color: green;\r\n  }\r\n\r\n  .empty {\r\n    background-color: white;\r\n  }\r\n}\r\n\r\n.hidden {\r\n  display: none !important;\r\n}\r\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -889,4 +993,4 @@ module.exports = styleTagTransform;
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
 /******/ }
 ]);
-//# sourceMappingURL=bundle3151a331d7266d9e23b9.js.map
+//# sourceMappingURL=bundlee0aa53a42d8cc8b937cf.js.map
